c8e6a328e798af8ff25e58a17f12c28c97c8379f
Julius Lehmann <jcslehmann@gmail.com>
2019-05-29 16:48:12 +0100

    finished a my-flutters-page

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ User.create!(username: "admin", first_name: "Joe", last_name: "Blogs", password:
 
 puts "created #{User.first.email} as an admin!"
 
-5.times do
+6.times do
   balance = (1..50).to_a.sample
   user = User.new(first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name, username: Faker::Food.dish,
@@ -40,8 +40,8 @@ puts "Users created!"
   odds = rand(0.1..0.9)
   proposition = Faker::Movies::HarryPotter.quote
   deadline =  Faker::Date.forward((1..20).to_a.sample)
-  proposer = User.all.sample
-  receiver = User.all.sample
+  proposer = User.first(3).sample
+  receiver = User.last(3).sample
   status = %w[pending confirmed unvalidated finished]
   likes = (1..100).to_a.sample
   Bet.create!(stake: stake, odds: odds, deadline: deadline, likes: likes,
