b2c77dfef19ca65e84b1df02ad127e0ba8b5161e
Julius Lehmann <jcslehmann@gmail.com>
2019-06-01 13:30:28 +0100

    updated controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,15 @@
 class PagesController < ApplicationController
   before_action :authenticate_user!
 
-
   def home
     @bets = Bet.all
     @bet = Bet.new
     @user = current_user
+    @live_bets = []
+    @bets.each do |b|
+      if b.status == "confirmed" || b.status == "finished" && current_user != b.proposer || current_user != b.receiver
+        @live_bets << b
+      end
+    end
   end
 end
