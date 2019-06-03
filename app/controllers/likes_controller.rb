457049bc8c70725a93975272d1036a7ea661327a
Julius Lehmann <jcslehmann@gmail.com>
2019-06-03 23:02:40 +0100

    closer

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,15 +1,14 @@
 class LikesController < ApplicationController
 before_action :find_like, only: [:destroy]
-
+after_action :verify_authorized, except: [:create, :destroy]
 
   def create
     if already_liked?
       flash[:notice] = "You can't like more than once"
     else
-      @bet.likes.create(user_id: current_user.id, like: true)
+      Like.new(user_id: current_user.id, bet_id: params[:id])
     end
-      redirect_to bet_path(@bet)
-      authorize @bet
+      redirect_to root_path
 
   end
 
