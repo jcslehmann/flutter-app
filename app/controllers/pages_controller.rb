f2905f2b9560ee0c215b3c60ef303a39605aca23
Benjamin Baranger <baranger.benjamin@gmail.com>
2019-06-05 12:59:50 +0100

    ajaxify like/dislike + fix minors bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,11 +3,8 @@ class PagesController < ApplicationController
   # before_action :authenticate_user!
 
   def home
-    @bets = Bet.all
-    @bet = Bet.new
-    @user = current_user
     @live_bets = []
-    @bets.each do |b|
+    Bet.all.each do |b|
       if b.status == "confirmed" || b.status == "finished" && current_user != b.proposer || current_user != b.receiver
         @live_bets << b
       end
